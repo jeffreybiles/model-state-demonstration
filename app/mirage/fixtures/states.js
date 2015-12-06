780c8b43d14e6a39a3a0639dde2fc360e5c81145
jeffreybiles <bilesjeffrey@gmail.com>
2015-12-05 21:51:58 -0600

    View state's flags, updated some incorrect flags

--- app/mirage/fixtures/states.js
+++ app/mirage/fixtures/states.js
@@ -3,81 +3,96 @@ export default [
     id: 1,
     name: 'root',
     parent: null,
-    children: [2, 6]
+    children: [2, 6],
+    flags: ['valid']
   }, {
     id: 2,
     name: 'deleted',
     parent: 1,
-    children: [3, 4, 5, 16]
+    children: [3, 4, 5, 16],
+    flags: ['valid', 'dirty', 'deleted', 'loaded']
   }, {
     id: 3,
     name: 'deleted.saved',
-    parent: 2
+    parent: 2,
+    flags: ['valid', 'deleted', 'loaded']
   }, {
     id: 4,
     name: 'deleted.uncommitted',
-    parent: 2
+    parent: 2,
+    flags: ['valid', 'dirty', 'deleted', 'loaded']
   }, {
     id: 5,
     name: 'deleted.inFlight',
-    parent: 2
+    parent: 2,
+    flags: ['valid', 'dirty', 'deleted', 'loaded', 'saving']
   }, {
     id: 6,
     name: 'empty',
-    parent: 1
+    parent: 1,
+    flags: ['valid', 'empty']
   }, {
     id: 7,
     name: 'loaded',
     parent: 1,
-    children: [8, 11, 12]
+    children: [8, 11, 12],
+    flags: ['valid', 'loaded']
   }, {
     id: 8,
     name: 'loaded.created',
     parent: 7,
-    children: [9, 10, 17]
+    children: [9, 10, 17],
+    flags: ['valid', 'dirty', 'loaded', 'new']
   }, {
     id: 9,
     name: 'loaded.created.uncommitted',
-    parent: 8
+    parent: 8,
+    flags: ['valid', 'dirty', 'loaded', 'new']
   }, {
     id: 10,
     name: 'loaded.created.inFlight',
-    parent: 8
+    parent: 8,
+    flags: ['valid', 'dirty', 'loaded', 'new', 'saving']
   }, {
     id: 11,
     name: 'loaded.saved',
-    parent: 7
+    parent: 7,
+    flags: ['valid', 'loaded']
   }, {
     id: 12,
     name: 'loaded.updated',
     parent: 7,
-    children: [13, 14, 18]
+    children: [13, 14, 18],
+    flags: ['valid', 'dirty', 'loaded']
   }, {
     id: 13,
     name: 'loaded.updated.uncommitted',
-    parent: 12
+    parent: 12,
+    flags: ['valid', 'dirty', 'loaded']
   }, {
     id: 14,
     name: 'loaded.updated.inFlight',
-    parent: 12
+    parent: 12,
+    flags: ['valid', 'dirty', 'loaded', 'saving']
   }, {
     id: 15,
     name: 'loading',
-    parent: 1
+    parent: 1,
+    flags: ['valid', 'loading']
   }, {
     id: 16,
     name: 'deleted.invalid',
     parent: 2,
-    isValid: false
+    flags: ['dirty', 'deleted', 'loaded']
   }, {
     id: 17,
     name: 'loaded.created.invalid',
     parent: 8,
-    isValid: false
+    flags: ['dirty', 'loaded']
   }, {
     id: 18,
     name: 'loaded.updated.invalid',
     parent: 12,
-    isValid: false
+    flags: ['dirty', 'loaded']
   }
 ]
--- app/models/state.js
+++ app/models/state.js
@@ -2,21 +2,29 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  parent: DS.belongsTo('state', {inverse: 'children'}),
-  children: DS.hasMany('state', {defaultValue: [], inverse: 'parent'}),
-  transitions: DS.hasMany('transition', {defaultValue: []}),
+  parent: DS.belongsTo('state', {inverse: 'children', async: true}),
+  children: DS.hasMany('state', {defaultValue: [], inverse: 'parent', async: true}),
+  transitions: DS.hasMany('transition', {defaultValue: [], async: true}),
 
-  isDeleted: DS.attr('boolean', {defaultValue: false}),
-  isEmpty: DS.attr('boolean', {defaultValue: false}),
-  isError: DS.attr('boolean', {defaultValue: false}),
-  isLoaded: DS.attr('boolean', {defaultValue: false}),
-  isLoading: DS.attr('boolean', {defaultValue: false}),
-  isNew: DS.attr('boolean', {defaultValue: false}),
-  isReloading: DS.attr('boolean', {defaultValue: false}),
-  isSaving: DS.attr('boolean', {defaultValue: false}),
-  isValid: DS.attr('boolean', {defaultValue: true}),
+  flags: DS.attr(),
+  isDeleted: isX('deleted'),
+  isDirty: isX('dirty'),
+  isEmpty: isX('empty'),
+  isError: isX('error'),
+  isLoaded: isX('loaded'),
+  isLoading: isX('loading'),
+  isNew: isX('new'),
+  isSaving: isX('saving'),
+  isValid: isX('valid'),
+  possibleFlags: ['isDeleted', 'isDirty', 'isEmpty', 'isError', 'isLoaded', 'isLoading', 'isNew',  'isSaving', 'isValid'],
 
   shortName: Ember.computed('name', function(){
     return this.get('name').split('.').get('lastObject')
   })
 });
+
+function isX(dependentKey) {
+  return Ember.computed(`flags.${dependentKey}`, function() {
+    return this.get('flags').includes(dependentKey);
+  });
+}
