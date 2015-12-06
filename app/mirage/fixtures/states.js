a2b9e13e63c464a4841e412b7791146753ac8cbc
jeffreybiles <bilesjeffrey@gmail.com>
2015-12-06 15:45:24 -0600

    root. added to the beginning of state names

--- app/mirage/fixtures/states.js
+++ app/mirage/fixtures/states.js
@@ -7,91 +7,91 @@ export default [
     flags: ['valid']
   }, {
     id: 2,
-    name: 'deleted',
+    name: 'root.deleted',
     parent: 1,
     children: [3, 4, 5, 16],
     flags: ['valid', 'dirty', 'deleted', 'loaded']
   }, {
     id: 3,
-    name: 'deleted.saved',
+    name: 'root.deleted.saved',
     parent: 2,
     flags: ['valid', 'deleted', 'loaded']
   }, {
     id: 4,
-    name: 'deleted.uncommitted',
+    name: 'root.deleted.uncommitted',
     parent: 2,
     flags: ['valid', 'dirty', 'deleted', 'loaded']
   }, {
     id: 5,
-    name: 'deleted.inFlight',
+    name: 'root.deleted.inFlight',
     parent: 2,
     flags: ['valid', 'dirty', 'deleted', 'loaded', 'saving']
   }, {
     id: 6,
-    name: 'empty',
+    name: 'root.empty',
     parent: 1,
     flags: ['valid', 'empty']
   }, {
     id: 7,
-    name: 'loaded',
+    name: 'root.loaded',
     parent: 1,
     children: [8, 11, 12],
     flags: ['valid', 'loaded']
   }, {
     id: 8,
-    name: 'loaded.created',
+    name: 'root.loaded.created',
     parent: 7,
     children: [9, 10, 17],
     flags: ['valid', 'dirty', 'loaded', 'new']
   }, {
     id: 9,
-    name: 'loaded.created.uncommitted',
+    name: 'root.loaded.created.uncommitted',
     parent: 8,
     flags: ['valid', 'dirty', 'loaded', 'new']
   }, {
     id: 10,
-    name: 'loaded.created.inFlight',
+    name: 'root.loaded.created.inFlight',
     parent: 8,
     flags: ['valid', 'dirty', 'loaded', 'new', 'saving']
   }, {
     id: 11,
-    name: 'loaded.saved',
+    name: 'root.loaded.saved',
     parent: 7,
     flags: ['valid', 'loaded']
   }, {
     id: 12,
-    name: 'loaded.updated',
+    name: 'root.loaded.updated',
     parent: 7,
     children: [13, 14, 18],
     flags: ['valid', 'dirty', 'loaded']
   }, {
     id: 13,
-    name: 'loaded.updated.uncommitted',
+    name: 'root.loaded.updated.uncommitted',
     parent: 12,
     flags: ['valid', 'dirty', 'loaded']
   }, {
     id: 14,
-    name: 'loaded.updated.inFlight',
+    name: 'root.loaded.updated.inFlight',
     parent: 12,
     flags: ['valid', 'dirty', 'loaded', 'saving']
   }, {
     id: 15,
-    name: 'loading',
+    name: 'root.loading',
     parent: 1,
     flags: ['valid', 'loading']
   }, {
     id: 16,
-    name: 'deleted.invalid',
+    name: 'root.deleted.invalid',
     parent: 2,
     flags: ['dirty', 'deleted', 'loaded']
   }, {
     id: 17,
-    name: 'loaded.created.invalid',
+    name: 'root.loaded.created.invalid',
     parent: 8,
     flags: ['dirty', 'loaded']
   }, {
     id: 18,
-    name: 'loaded.updated.invalid',
+    name: 'root.loaded.updated.invalid',
     parent: 12,
     flags: ['dirty', 'loaded']
   }
